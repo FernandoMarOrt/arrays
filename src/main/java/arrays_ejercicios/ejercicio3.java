c7a20243dd1bb75df759830dea10ff469aa1a6ea
FernandoMarOrt <fernandomartinezortiz003@gmail.com>
2022-12-04 17:42:30 +0100

    ejercici03

--- src/main/java/arrays_ejercicios/ejercicio3.java
+++ src/main/java/arrays_ejercicios/ejercicio3.java
@@ -4,16 +4,104 @@
  */
 package arrays_ejercicios;
 
+import java.util.Arrays;
+
 /**
  *
  * @author fer
  */
 public class ejercicio3 {
-    
+
     public static void main(String[] args) {
+
+        //array de tamaño 20
+        double[] numeros = new double[20];
+        boolean iguales = false;
         
-        double [] numeros = new double[10];
+        //Rellena el array con el valor 7.5 en todas sus posiciones.
+        System.out.println("Rellena el array con el valor 7.5 en todas sus posiciones.");
+        
+        Arrays.fill(numeros, 0, 20, 7.5);
+        
+        
+        for (double i : numeros ) {
+            System.out.print(i + "|");
+        }
+
+        
+        
+        //Realiza una copia del array anterior en un nuevo array.
+        double[] numeros2 = Arrays.copyOf(numeros, 20);
+        
+           
+        
+        //Imprime ambos arrays. No se usa clase Arrays
+        System.out.println("");
+        System.out.println("Imprime ambos arrays. No se usa clase Arrays");
+        
+        System.out.println("Array original");
+        for (double i : numeros) {
+            System.out.print(i + "|");
+        }
+        
+        System.out.println("");
+        System.out.println("Array copia");
+        for (double i : numeros2) {
+            System.out.print(i + "|");
+        }
+        
+        
+
+        //Compara ambos arrays, mostrando si son iguales.
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Compara ambos arrays, mostrando si son iguales.");
+ 
+        iguales = Arrays.equals(numeros, numeros);
+
+        System.out.println("¿El array numeros es igual a el array numeros2? " + iguales);
+
+
+
+
+        //Introduce el valor 6.3 en una posición aleatoria válida de uno de los arrays. No se usa clase Arrays
+        System.out.println("");
+        System.out.println("Introduce el valor 6.3 en una posición aleatoria válida de uno de los arrays.");
+ 
+        numeros[0] = 6.3;
+
+        for (double i : numeros) {
+            System.out.print(i + "|");
+        }
+        
+        
+        
+         //Imprime ambos arrays.
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Imprime ambos arrays. No se usa clase Arrays");
+        
+        System.out.println("Array original");
+        for (double i : numeros) {
+            System.out.print(i + "|");
+        }
+        
+        System.out.println("");
+        System.out.println("Array copia");
+        for (double i : numeros2) {
+            System.out.print(i + "|");
+        }
+
+        //Vuelve a compararlos, indicando si son iguales o no.
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Vuelve a compararlos, indicando si son iguales o no.");
+        //##
+        iguales = Arrays.equals(numeros, numeros2);
+        //##
+        System.out.println("¿Són los arrays iguales?: " + iguales);
         
     }
     
+
 }
